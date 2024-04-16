88e94bb9678ff08d110c324fe3d7ad396ebcccfa
Alfredo Silva <a.alfredops@gmail.com>
2024-04-16 14:09:34 -0400

    Finishing global theme provider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,11 +2,12 @@ import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+    return (
+        <div>
+            <UserButton afterSignOutUrl="/" />
+            <h1>HOME PAGE</h1>
+        </div>
+    );
 };
 
 export default Home;
