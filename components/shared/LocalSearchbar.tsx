3f6e714a686a3482e1f8da367f5721a035036bb8
Alfredo Silva <a.alfredops@gmail.com>
2024-04-23 11:39:36 -0400

    syncing

---
+++ components/shared/LocalSearchbar.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Input } from "../ui/input";
+import Image from "next/image";
+
+interface CustomInputProps {
+    route: string;
+    iconPosition: string;
+    imgSrc: string;
+    placeholder: string;
+    otherClasses: string;
+}
+
+const LocalSearchbar = ({
+    route,
+    iconPosition,
+    imgSrc,
+    placeholder,
+    otherClasses,
+}: CustomInputProps) => {
+    return (
+        // TODO: Set a max width for this compoenent;
+        <div
+            className={`background-light800_darkgradient relative  flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+        >
+            {iconPosition === "left" && (
+                <Image
+                    src={imgSrc}
+                    alt="search"
+                    width={24}
+                    height={24}
+                    className="cursor-pointer"
+                />
+            )}
+            <Input
+                className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+                type="text"
+                value={""}
+                onChange={() => {}}
+                placeholder={placeholder}
+            />
+            {iconPosition === "right" && (
+                <Image
+                    src={imgSrc}
+                    alt="search"
+                    width={24}
+                    height={24}
+                    className="cursor-pointer"
+                />
+            )}
+        </div>
+    );
+};
+
+export default LocalSearchbar;
--- components/shared/navbar/search/GlobalSearch.tsx
+++ components/shared/navbar/search/GlobalSearch.tsx
@@ -1,24 +1,54 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
-const GlobalSearch = () => {
+interface CustomInputProps {
+    route: string;
+    iconPosition: string;
+    imgSrc: string;
+    placeholder: string;
+    otherClasses: string;
+}
+
+const GlobalSearch = ({
+    route,
+    iconPosition,
+    imgSrc,
+    placeholder,
+    otherClasses,
+}: CustomInputProps) => {
     return (
-        <div className="relative w-full max-w-[600px] max-lg:hidden">
-            <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
+        // TODO: Set a max width for this compoenent;
+        <div
+            className={`background-light800_darkgradient relative  flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+        >
+            {iconPosition === "left" && (
                 <Image
-                    src="/assets/icons/search.svg"
+                    src={imgSrc}
                     alt="search"
                     width={24}
                     height={24}
                     className="cursor-pointer"
                 />
-                <Input
-                    className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
-                    type="text"
-                    placeholder="Search globally"
+            )}
+            <Input
+                className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+                type="text"
+                value={""}
+                onChange={() => {}}
+                placeholder={placeholder}
+            />
+            {iconPosition === "right" && (
+                <Image
+                    src={imgSrc}
+                    alt="search"
+                    width={24}
+                    height={24}
+                    className="cursor-pointer"
                 />
-            </div>
+            )}
         </div>
     );
 };
