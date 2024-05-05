d8779d628b7af8eeddf07ab0e665becba8938be9
Alfredo Silva <a.alfredops@gmail.com>
2024-05-05 19:06:22 -0400

    Prettier Configs

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,18 +5,18 @@ import NavBar from "@/components/shared/navbar/NavBar";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <main className="relative background-light850_dark100">
-            <NavBar />
-            <div className="flex">
-                <LeftSidebar />
-                <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14 ">
-                    <div className="mx-auto w-full max-w-5xl">{children}</div>
-                </section>
-                <RigthSidebar />
-            </div>
-        </main>
-    );
+  return (
+    <main className="relative background-light850_dark100">
+      <NavBar />
+      <div className="flex">
+        <LeftSidebar />
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14 ">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+        <RigthSidebar />
+      </div>
+    </main>
+  );
 };
 
 export default layout;
